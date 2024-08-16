270739d30a9e94f63933adb9f335669e989ed47e
sunny.park <seolseee@gmail.com>
2024-08-17 00:45:20 +0900

    feat : spring-core-1 - 의존성 주입(필드 주입 방식)

--- spring-core-1/initial/src/main/java/cholog/di/FieldInjection.java
+++ spring-core-1/initial/src/main/java/cholog/di/FieldInjection.java
@@ -9,6 +9,9 @@ public class FieldInjection {
     /*
     FieldInjection으로 InjectionBean 주입받기
      */
+    // 의존성 주입 : 필드 주입 방식
+    // 필드 위에 @Autowired 어노테이션을 붙여줌으로써, 스프링이 해당 빈을 필드에 직접 주입해주는 방식임
+    @Autowired
     private InjectionBean injectionBean;
 
     public String sayHello() {
