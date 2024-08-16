7c0a4e5aa213905485ed96b330383c097e148fd3
sunny.park <seolseee@gmail.com>
2024-08-17 00:45:12 +0900

    feat : spring-core-1 - 의존성 주입(세터 주입 방식)

--- spring-core-1/initial/src/main/java/cholog/di/SetterInjection.java
+++ spring-core-1/initial/src/main/java/cholog/di/SetterInjection.java
@@ -11,6 +11,14 @@ public class SetterInjection {
     Setter Injection으로 InjectionBean 주입받기
      */
 
+    // 의존성 주입 : 세터 주입 방식
+    // 객체가 생성된 후, 스프링 컨테이너가 세터 메서드를 호출할 때 의존성이 주입됨. 즉, 세터 메서드가 실행할 때 의존성이 주입됨
+    // 세터 메서드의 호출은 스프링 프레임 워크가 자동으로 세터 메서드를 인식하고 호출함.
+    // 스프링이 세터 메서드를 인식하고 호출하는 방식은 @Autowired 어노테이션과 세터 메서드의 특성인 메서드의 이름(set으로 시작)과 하나의 매개변수를 받아 필드 값을 설정하는 메서드 패턴을 통해 세터 메서드임을 인식함
+    @Autowired
+    public void setInjectionBean(InjectionBean injectionBean) {
+        this.injectionBean = injectionBean;
+    }
     public String sayHello() {
         return injectionBean.hello();
     }
