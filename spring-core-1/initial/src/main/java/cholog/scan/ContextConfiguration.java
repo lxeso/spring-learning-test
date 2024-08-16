0540dfca3584845dc61ef255cc6a753169b4b97f
sunny.park <seolseee@gmail.com>
2024-08-17 00:46:42 +0900

    feat : spring-core-1 - @ComponentScan (스프링 애플리케이션이 어떤 패키지에서 스프링 빈(클래스)을 스캔하고 자동으로 등록할지 지정해주는 역할)

--- spring-core-1/initial/src/main/java/cholog/scan/ContextConfiguration.java
+++ spring-core-1/initial/src/main/java/cholog/scan/ContextConfiguration.java
@@ -1,10 +1,12 @@
 package cholog.scan;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 /*
 ComponentScan에 대해 학습하고, ComponenetScanBean을 Bean으로 등록하기
  */
+@ComponentScan(basePackages = "cholog.scan") // 스프링 애플리케이션에서 어떤 패키지 안에 있는 클래스를 스캔하여 스프링 빈으로 등록할지를 지정하는 어노테이션
 public class ContextConfiguration {
 }
