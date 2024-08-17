dcf4b6788873ebc40665a0aabe56f4e5ee3f416a
sunny.park <seolseee@gmail.com>
2024-08-17 23:56:52 +0900

    feat : spring-core-2 -  @propertysource를 사용해 로드한 프로퍼티 파일의 값을 @value 어노테이션을 통해 주입
    (cherry picked from commit 0f6e61a0d4e83a9d568db67ada3853c201688088)

--- spring-core-2/initial/src/main/java/cholog/property/config/AuthConfig.java
+++ spring-core-2/initial/src/main/java/cholog/property/config/AuthConfig.java
@@ -1,11 +1,16 @@
 package cholog.property.config;
 
 import cholog.property.JwtTokenKeyProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
+@Configuration
 public class AuthConfig {
     // TODO: application.properties의 security.jwt.token.secret-key 값을 활용하여 JwtTokenKeyProvider를 빈으로 등록하기
-    public JwtTokenKeyProvider jwtTokenKeyProvider() {
-        return new JwtTokenKeyProvider("");
+    @Bean
+    public JwtTokenKeyProvider jwtTokenKeyProvider(@Value("${security.jwt.token.secret-key}") String secretKey) {
+        return new JwtTokenKeyProvider(secretKey);
     }
 }
