7edeefaa2c8cc997ed5dd7b86bba990f7dd5b41a
sunny.park <seolseee@gmail.com>
2024-08-17 23:56:26 +0900

    feat : spring-core-2 - @propertysource 어노테이션을 사용해 프로퍼티 파일을 로드하고 Environment를 사용해 프로퍼티 값을 읽어오기,  @propertysource를 사용해 로드한 프로퍼티 파일의 값을 @value 어노테이션을 통해 주입

--- spring-core-2/initial/src/main/java/cholog/property/config/PropertySourceConfig.java
+++ spring-core-2/initial/src/main/java/cholog/property/config/PropertySourceConfig.java
@@ -2,10 +2,16 @@ package cholog.property.config;
 
 import cholog.property.GoogleDriveRestClient;
 import cholog.property.GoogleMapsRestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 // TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
+@Configuration
 // TODO: ext-api.properties 파일을 활용하기 위한 설정 추가하기
+@PropertySource("ext-api.properties")
 public class PropertySourceConfig {
 
     private final Environment env;
@@ -16,13 +22,15 @@ public class PropertySourceConfig {
 
     // TODO: ext-api.properties의 google.api.endpoint 값을 Environment를 사용해서 가져오기
     // TODO: 위 endpoint 값을 사용하여 GoogleMapsRestClient를 빈으로 등록하기
+    @Bean
     public GoogleMapsRestClient googleMapsRestClient() {
-        return new GoogleMapsRestClient("");
+        return new GoogleMapsRestClient(env.getProperty("google.api.endpoint"));
     }
 
     // TODO: ext-api.properties의 google.api.endpoint 값을 어노테이션을 사용해서 가져오기
     // TODO: 위 endpoint 값을 사용하여 GoogleMapsRestClient를 빈으로 등록하기
-    public GoogleDriveRestClient googleDriveRestClient() {
-        return new GoogleDriveRestClient("");
+    @Bean
+    public GoogleDriveRestClient googleDriveRestClient(@Value("${google.api.endpoint}") String endPoint) {
+        return new GoogleDriveRestClient(endPoint);
     }
 }
