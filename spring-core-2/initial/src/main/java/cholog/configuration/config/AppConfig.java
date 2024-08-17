6515a20dffb8d37d7e884cb113c0a0df8437227d
sunny.park <seolseee@gmail.com>
2024-08-17 23:55:48 +0900

    feat : spring-core-2 -  @configuration 어노테이션이 있는 클래스 내의 메서드는 같은 클래스 내의 다른 @bean 메서드를 호출하여 빈 간의 의존성을 정의
    (cherry picked from commit 7f1c780aa87c7de3ccb3f0d72a401823fae1cffa)

--- spring-core-2/initial/src/main/java/cholog/configuration/config/AppConfig.java
+++ spring-core-2/initial/src/main/java/cholog/configuration/config/AppConfig.java
@@ -2,18 +2,23 @@ package cholog.configuration.config;
 
 import cholog.configuration.AuthService;
 import cholog.configuration.AuthenticationPrincipalArgumentResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
+@Configuration // 스프링 애플리케이션의 설정 클래스임을 나타냄
 public class AppConfig {
 
     // TODO: AuthService 빈을 등록하기
+    @Bean
     public AuthService authService() {
-        return null;
+        return new AuthService();
     }
 
     // TODO: AuthenticationPrincipalArgumentResolver를 빈 등록하고 authService에 대한 의존성을 주입하기
+    @Bean
     public AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver() {
-        return null;
+        return new AuthenticationPrincipalArgumentResolver(authService());
     }
 
 }
