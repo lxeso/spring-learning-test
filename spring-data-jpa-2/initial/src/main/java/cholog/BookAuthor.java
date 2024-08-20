a102baa19cdfd891f9f6813f4ce999c5bc6c5832
sunny.park <seolseee@gmail.com>
2024-08-20 23:51:39 +0900

    feat : spring-data-jpa-2 -  @manytoone 어노테이션을 활용

--- spring-data-jpa-2/initial/src/main/java/cholog/BookAuthor.java
+++ spring-data-jpa-2/initial/src/main/java/cholog/BookAuthor.java
@@ -1,7 +1,31 @@
 package cholog;
 
-public class BookAuthor {
-    public BookAuthor(Book book, Author author) {
+import jakarta.persistence.*;
 
+@Entity
+public class BookAuthor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Book book;
+
+    @ManyToOne
+    private Author author;
+
+    public BookAuthor(Book book, Author author) {
+        this.book = book;
+        this.author = author;
     }
+
+    public  BookAuthor() {
+    }
+
+    public Long getId() { return id; }
+
+    public Book getBook() { return book; }
+
+    public Author getAuthor() { return author; }
+
 }
